9752ea60d6619403919c77a54f33f02350318e27
Christian Duus <duus92@web.de>
2025-10-01 15:06:23 +0200

    project cleared, fitst component created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BaseCard></BaseCard>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseCard from './components/BaseCard.vue';
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {BaseCard
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+html{
 }
+body{
+  margin: 0;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  box-sizing: border-box;
+
+
+}
+
+#app{
+  min-height: 100vh;
+  width: 100vw;
+}
+
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="BaseCard">
+
+        <slot></slot>
+    </div>
+
+</template>
+
+<script>
+export default{
+    name: 'BaseCard',
+    components:{
+
+    }
+}
+
+</script>
+
+<style>
+.BaseCard{
+    border-radius: 16px;
+    background-color: #011F35;
+    padding: 24px 32px;
+
+}
+
+</style>
