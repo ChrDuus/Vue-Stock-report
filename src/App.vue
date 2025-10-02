a9968cae2e21bbed180f5430759175d652e96042
Christian Duus <duus92@web.de>
2025-10-02 16:51:04 +0200

    Service for treceiving data from api added

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,24 @@
 
 <script>
 import BaseCard from './components/BaseCard.vue';
+import { stockService } from './services/stockService';
 
 
 export default {
   name: 'App',
   components: {BaseCard
+  },
+  async created(){
+    this.data = await stockService.fetchData('$AAPL')
+    console.log(this.data);
+    
   }
 }
 </script>
 
 <style>
 
-html{
-}
+
 body{
   margin: 0;
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
